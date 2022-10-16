37e7696a18108678c668413c7e1dd235891f1d9c
Gerson Lamela <gersonlamela7@gmail.com>
2022-10-16 17:01:37 +0100

    ci: Add publish storybook workflow

--- storybook-static/assets/preview.7f27d41c.js
+++
@@ -1,2 +0,0 @@
-import{t as o}from"./index.f830803f.js";import"./iframe.d21aacb2.js";import"./es.map.constructor.26c58445.js";import"./es.number.to-fixed.057ad5f5.js";import"./index.7810f6c1.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
-//# sourceMappingURL=preview.7f27d41c.js.map
