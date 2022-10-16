cfa894d5b55804187ba100d94d75d8e56aeae55f
Gerson Lamela <gersonlamela7@gmail.com>
2022-10-16 17:24:46 +0100

    ci: Add publish storybook workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+	stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)', '../src/**/*.stories.mdx'],
 	addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-a11y'],
 	framework: '@storybook/react',
 	core: {
@@ -7,12 +7,14 @@ module.exports = {
 	},
 	features: {
 		storyStoreV7: true,
+		interactionsDebugger: true,
 	},
+	staticDirs: ['../public'],
+
 	viteFinal: (config, {configType}) => {
 		if (configType === 'PRODUCTION') {
-			config.base = '/ignite-lab-design-system/';
+			config.base = '/Ignite-Labs-Design-System/';
 		}
-
 		return config;
 	},
 };
